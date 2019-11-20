7499c533da09455c7186452b3f8210e3af871a19
hietacoke <hieta.coke1192@gmail.com>
2019-11-20 15:24:10 +0900

    htaccess追加

--- RealVideo/settings.py
+++ RealVideo/settings.py
@@ -38,12 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cloudinary_storage',
-    'cloudinary',
     'bootstrap4',
     'django_cleanup',
     'widget_tweaks',
     'video',
+    'cloudinary_storage',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
